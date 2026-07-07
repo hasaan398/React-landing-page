92ccf1591b29ddd2c261f9b64932e582c9fea191
hassaan <hassanatif020@gmail.com>
2026-07-07 15:00:26 +0500

    Your message

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/Logo.png";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 
